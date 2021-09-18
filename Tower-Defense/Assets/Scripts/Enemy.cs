30dc91f8bcf5d18efdb2cd44ff687f6e1e8ba6e2
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-18 12:47:57 +0700

    can shoot, but healthbar doesn't decreased

--- Tower-Defense/Assets/Scripts/Enemy.cs
+++ Tower-Defense/Assets/Scripts/Enemy.cs
@@ -11,9 +11,62 @@ public class Enemy : MonoBehaviour
 
     private int _currentHealth;
 
+    public Vector3 TargetPosition { get; private set; }
+    public int CurrentPathIndex { get; private set; }
+
     private void OnEnable()
     {
         _currentHealth = _maxHealth;
         _healthFill.size = _healthBar.size;
+    }
+
+    public void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, _moveSpeed * Time.deltaTime);
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        TargetPosition = targetPosition;
+        _healthBar.transform.parent = null;
+
+        Vector3 distance = TargetPosition - transform.position;
+        if(Mathf.Abs(distance.y) > Mathf.Abs(distance.x))
+        {
+            if(distance.y > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -90f));
+            }
+        }
+        else
+        {
+            if(distance.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
+            }
+        }
+        _healthBar.transform.parent = transform;
+    }
+
+    public void SetCurrentPathIndex(int currentIndex)
+    {
+        CurrentPathIndex = currentIndex;
+    }
+
+    public void ReduceEnemyHealth(int damage)
+    {
+        _currentHealth -= damage;
+        if(_currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
